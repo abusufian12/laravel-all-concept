e45f01f601d9a5c8d0d31ff3449de2f20da8cd6a
DESKTOP-RLQEV9L\sufian <abusufian12@gmail.com>
2022-07-08 12:01:45 +0900

    add

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_login_form()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    public function test_user_duplication(){
+        
+    }
+}
